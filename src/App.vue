0c81d951496986859de150ffad848625dbdd3442
daisylys <rgdmlys@126.com>
2020-02-17 18:38:27 +0800

    vue-cli3
    router vuex element

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <ul class="navbar">
+      <!-- 使用 router-link 组件来导航. 通过传入 `to` 属性指定链接. -->
+      <router-link tag="li" to="/about">about</router-link>
+      <router-link tag="li" to="/">Home</router-link>
+      <router-link tag="li" to="/my">My</router-link>
+    </ul>
+    <!-- 路由出口:路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  <p>aaaaaaa</p>
+    <el-button>el-button</el-button>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
-#nav {
-  padding: 30px;
+ul{
+  display: flex;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+li {
+  flex: 1;
+  font-size: 12px;
+  list-style: none;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.router-link-exact-active.router-link-active {
+  color: #ffcccc;
 }
 </style>
